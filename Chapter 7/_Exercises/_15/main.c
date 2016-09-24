95530d21137f4e9b37099b3d24b25cfae6751431
Merrick R <merrick@ctsfla.com>
2016-09-24 19:47:52 -0400

    Add 64 and 128 bit integer definitions

--- Chapter 7/_Exercises/_15/main.c
+++ Chapter 7/_Exercises/_15/main.c
@@ -11,10 +11,17 @@ int main(void)
 	typedef int8_t Int8;
 	typedef int16_t Int16;
 	typedef int32_t Int32;
+	// also..
+	typedef int64_t Int64;
+	typedef __int128_t Int128;
 
+	printf("Size of integers in bytes:\n");
 	printf("Size of Int8: %zu\n", sizeof(Int8));
 	printf("Size of Int16: %zu\n", sizeof(Int16));
 	printf("Size of Int32: %zu\n", sizeof(Int32));
+	// also..
+	printf("Size of Int64: %zu\n", sizeof(Int64));
+	printf("Size of Int128: %zu\n", sizeof(Int128));
 
 
 	return 0;
