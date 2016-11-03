aaa4a6000eb5c7d804165cb8ee8761c3ccc60a5c
Merrick R <merrick@ctsfla.com>
2016-11-03 14:32:05 -0400

    Chapter 8 Programming Projects 7 & 8

---
+++ Chapter 8/7/5x5_sum.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define SQUARE_MATRIX_NUM 5
+
+/* 
+Write a program that reads a 5 x 5 array of integers and then prints the
+row sums and the column sums.
+*/
+
+int main(void)
+{
+	int sum = 0, row_num, col_num, square_matrix[SQUARE_MATRIX_NUM][SQUARE_MATRIX_NUM] = {0};
+
+	for(row_num = 0; row_num < SQUARE_MATRIX_NUM; row_num++)
+	{
+		printf("Enter row %d: ", row_num+1);
+		for(col_num = 0; col_num < SQUARE_MATRIX_NUM; col_num++)
+			scanf("%d", &square_matrix[row_num][col_num]);
+	}
+
+	printf("\nRow totals:");
+	for(row_num = 0; row_num < SQUARE_MATRIX_NUM; row_num++)
+	{
+		for(col_num = 0; col_num < SQUARE_MATRIX_NUM; col_num++)
+			sum += square_matrix[row_num][col_num];
+		printf(" %d", sum);
+		sum = 0;
+	}
+
+	printf("\nColumn totals:");
+	for(col_num = 0; col_num < SQUARE_MATRIX_NUM; col_num++)
+	{
+		for(row_num = 0; row_num < SQUARE_MATRIX_NUM; row_num++)
+			sum += square_matrix[row_num][col_num];
+		printf(" %d", sum);
+		sum = 0;
+	}
+
+	return 0;
+}
---
+++ Chapter 8/8/student_quizzes.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define STUDENTS_AND_QUIZZES 5
+
+/*
+Modify Programming Project 7 so that it prompts for five quiz grades for
+each of five students, then computes the total score and average score
+for each student, and the average score, high score, and low score for
+each quiz.
+*/
+
+int main(void)
+{
+	int highest_grade = 0, lowest_grade = 100, total_score = 0,
+		student_num, quiz_num, square_matrix[STUDENTS_AND_QUIZZES][STUDENTS_AND_QUIZZES] = {0};
+
+	for(student_num = 0; student_num < STUDENTS_AND_QUIZZES; student_num++)
+	{
+		printf("Enter Student %d Quiz Grades: ", student_num+1);
+		for(quiz_num = 0; quiz_num < STUDENTS_AND_QUIZZES; quiz_num++)
+			scanf("%d", &square_matrix[student_num][quiz_num]);
+	}
+
+	printf("\nStudent Data\n------------\n");
+	for(student_num = 0; student_num < STUDENTS_AND_QUIZZES; student_num++)
+	{
+		for(quiz_num = 0; quiz_num < STUDENTS_AND_QUIZZES; quiz_num++)
+			total_score += square_matrix[student_num][quiz_num];
+		printf("Student #%d - Total: %d  Average: %d%%\n", student_num+1, total_score, total_score / STUDENTS_AND_QUIZZES);
+		total_score = 0;
+	}
+
+	total_score = 0;
+
+	printf("\nQuiz Data\n---------\n");
+	for(quiz_num = 0; quiz_num < STUDENTS_AND_QUIZZES; quiz_num++)
+	{
+		for(student_num = 0; student_num < STUDENTS_AND_QUIZZES; student_num++)
+		{
+			total_score += square_matrix[student_num][quiz_num];
+			if(square_matrix[student_num][quiz_num] > highest_grade)
+				highest_grade = square_matrix[student_num][quiz_num];
+			if(square_matrix[student_num][quiz_num] < lowest_grade)
+				lowest_grade = square_matrix[student_num][quiz_num];
+		}
+		printf("Quiz #%d - Average: %d%%  Lowest: %d%%  Highest: %d%%\n",
+			quiz_num+1, total_score / STUDENTS_AND_QUIZZES, lowest_grade, highest_grade);
+		total_score = 0;
+		highest_grade = 0;
+		lowest_grade = 100;
+	}
+
+	return 0;
+}
